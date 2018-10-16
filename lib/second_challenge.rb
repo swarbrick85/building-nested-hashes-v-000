e0f133ff155afb090c421b7e7f976b67c3bb5346
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-16 10:12:43 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,6 +1,6 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {patriarch: {}, matriarch: {}, hero: {}, friends: []
+    :montague => {patriarch: {}, matriarch: {}, hero: {}, hero_friends: []
       
       }, 
     :capulet => {patriarch: {}, matriarch: {}, heroine: {}, heroine_friends: []
