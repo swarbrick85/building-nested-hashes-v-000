eba3e4df82407809249a4b2e291fc4b4d8eaab89
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-16 10:09:24 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,10 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
+    :montague => {patriarch: , matriarch: , hero: , friends: 
+      
       }, 
     :capulet => {
-      #your key/value pairs here!
+      
       }}
 
   
